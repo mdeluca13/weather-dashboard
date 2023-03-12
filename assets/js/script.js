147915f66e78768c84c794936c8177e41c7d51de
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-03-12 15:19:26 -0400

    added forecast sections and built the 5 day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,92 @@
-// https://api.openweathermap.org/data/2.5/forecast?lat=45.4215&lon=75.6972&appid=8e6644460f79289a6c57818a06a12a8d
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=8e6644460f79289a6c57818a06a12a8d
-//'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=8e6644460f79289a6c57818a06a12a8d'
-//http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=efca5b156605df37fb4cc956b882392d
-var latLonURL = 'http://api.openweathermap.org/geo/1.0/direct?';
-var weatherURL = 'http://api.openweathermap.org/data/2.5/forecast?';
-var apiid = '8e6644460f79289a6c57818a06a12a8d';
+// declaring variables
 var cityInput = $('#city-input');
 var searchBtn = $('#search-btn');
 var localStorageData;
+var searchList = $('#search-list');
+var createButton;
+var localStorageLength;
+var latitude;
+var longitude;
+var cityName;
+var temp;
+var wind;
+var humidity;
+var dayDisplay = $('#day-forecast');
+var fiveDayDisplay = $('#five-day-forecast');
 
+//Checking if local storage is empty or display previous searches 
+if (localStorage.length === 0) {
+    console.log('empty');
+}
+else {
+    for (var i = 0; i < localStorage.length; i++) {
+        createButton = $("<button class='btn btn-secondary prev-search'>" + JSON.parse(localStorage.getItem(i)) + "</button>")
+        searchList.append(createButton); 
+    }
+}
+
+//search button on click function to create the previous search buttons, calling getWeather and Saving to local storage
 searchBtn.on('click', function () {
-    console.log(cityInput.val());
+    if (cityInput.val() === '') {
+        console.log('no text')
+    }
+    else {
+        createButton = $("<button class='btn btn-secondary prev-search'>" + cityInput.val() + "</button>")
+        searchList.append(createButton);
+        getWeather(cityInput.val());
+        saveLastCity();
+    }
+
 });
 
-// fetch(latLonURL, {
-//     q: 'Ottawa,CA',
-//     appid: '8e6644460f79289a6c57818a06a12a8d',
-// })
-//     .then (function (response) {
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     });
+//click listener on previous searches to search that city
+$('.prev-search').click(function() {
+    getWeather($(this).text());
+    console.log($(this).text());
+})
 
+// get weather function
+function getWeather(input) {
 
-// fetch lat and lon first
-// then get forecast
-function getLatLon () {
-    fetch(latLonURL, 
-        {
-        q: 'London',
-        appid: apiid,
-        limit: 5,
-    })
+    //fetching lat and long for weather search
+    fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + input + '&limit=1&appid=8e6644460f79289a6c57818a06a12a8d')
+
     .then (function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        latitude = data[0].lat;
+        longitude = data[0].lon;
+
+        //fetching 5 day forecast weather data from lat and long
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=metric&appid=8e6644460f79289a6c57818a06a12a8d')
+        .then (function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (i = 0; i < 5; i++) {
+                $('#day-' + (i+1) + '-date').text(data.list[i].dt_txt);
+                $('#day-' + (i+1) + '-icon').attr('src', 'https://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '.png');
+                $('#day-' + (i+1) + '-icon').attr('alt', data.list[i].weather[0].main + ' icon');
+                $('#day-' + (i+1) + '-temp').text('Temp: ' + data.list[i].main.temp + '°C');
+                $('#day-' + (i+1) + '-wind').text('Temp: ' + data.list[i].wind.speed + 'k/h');
+                $('#day-' + (i+1) + '-humidity').text('Temp: ' + data.list[i].main.humidity + '%');
+            }
+            console.log(data);
+            cityName = data.city.name;
+
+            // adding 5 day forecast display
+             
+        });
     });
-};
-getLatLon();
+
+    // adding 5 day forecast display
+
+    
+
+}
+
+function saveLastCity() {
+    localStorageLength = localStorage.length;
+    localStorage.setItem(localStorageLength, JSON.stringify(cityInput.val()));
+}
